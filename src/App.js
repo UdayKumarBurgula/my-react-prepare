5638bcbf5856212c0ba773a04dd95e8f35d5d8cc
udaykumarburgula <udaykumar.burgula2k@gmail.com>
2025-12-08 18:37:14 +0530

    updated

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to My React App!</h1>
+      <p>This is a functional component.</p>
+    </div>
+  );
+}
+
+export default App;
