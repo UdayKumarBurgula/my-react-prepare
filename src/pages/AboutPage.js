14ee864c6822ea818bfa30e9f4775ba80395094e
udaykumarburgula <udaykumar.burgula2k@gmail.com>
2025-12-08 20:09:05 +0530

    Create a Counter Component.

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,16 +1,25 @@
 ﻿// src/pages/AboutPage.js
-import React from "react";
+import React, { useState} from "react";
 import { useTranslation } from "react-i18next";
 
 function AboutPage() {
   // ✅ Use about namespace + fallback to "common"
   const { t } = useTranslation(["about", "common"]);
 
+  const [count, setCount] = useState(0);
+
   return (
     <div>
       <h2>{t("title")}</h2>
       <p>{t("content")}</p>
       <p>{t("more")}</p>
+
+      <h2> Counter from About Page </h2>
+
+      <p> {count}</p>
+
+      <button onClick={() => setCount(count+1)} >Increment</button>
+      <button onClick={() => setCount(count-1)}>Decrement</button>
     </div>
   );
 }
