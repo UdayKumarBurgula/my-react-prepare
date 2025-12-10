e5e42f37d4074f78649105b14f635fff3f6f386d
udaykumarburgula <udaykumar.burgula2k@gmail.com>
2025-12-10 16:27:05 +0530

    Implement a Star Rating Component

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -7,17 +7,12 @@ const StarRating = ({ totalStars = 5 }) => {
     const [rating, setRating] = useState(0);
 
     return (
-        <div>
-            {[...Array(totalStars)].map((star, index) => {
+        <div> 
+            {                
+                [...Array(totalStars)].map((star, index) => {
                 const starValue = index + 1;
                 return (
-                    <span
-                        key={index}
-                        onClick={() => setRating(starValue)}
-                        style={{ cursor: 'pointer', color: starValue <= rating ? 'gold' : 'gray' }}
-                    >
-                        ★
-                    </span>
+                    <div>*</div>
                 );
             })}
         </div>
