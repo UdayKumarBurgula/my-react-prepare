6871ec614eac25fdde796a412c2de5cf64f42cb1
udaykumarburgula <udaykumar.burgula2k@gmail.com>
2025-12-13 09:08:34 +0530

    Counter test - useState

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -7,40 +7,24 @@ import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const Counter = () => {
+    const [count, setCount] = useState(0);
+
+    return (<div>
+        <h2>Count: {count}</h2>
+        <button onClick={() => setCount(count+1) }>Increment</button>
+        </div>)
+}
 
 function AboutPage() {
   // ✅ Use about namespace + fallback to "common"
     const { t } = useTranslation(["about", "common"]);
 
-    const [message, setMessage] = useState("");
-    const [chat, setChat] = useState([]);
-
-    useEffect(() => {
-        socket.on("receive_message", (data) => {
-            setChat((prev) => [...prev, data]);
-        });
-    }, []);
-
-    const sendMessage = () => {
-        socket.emit("send_message", message);
-        setMessage("");
-    };
-
+   
   return (
       <div>
-          <h1>Real-Time Chat</h1>
-          <input
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              placeholder="Type message..."
-          />
-          <button onClick={sendMessage}>Send</button>
-          <div>
-              {chat.map((msg, i) => (
-                  <p key={i}>{msg}</p>
-              ))}
-          </div>
+          <h1>useState program</h1>
+          <Counter></Counter>
       </div>
   );
 }
